ca3813dcc8eccba547aec1b2e04477639f330196
vineethvangala <acharya.vineeth@gmail.com>
2023-09-16 12:43:30 +0530

    Updates

---
+++ myScript.js
@@ -0,0 +1,25 @@
+function calculateExperience() {
+    today = new Date();
+    tcs_start = new Date(2018,0,18);
+    tcs_end = new Date(2021,5,4);
+    boeing_start = new Date(2021,5,7);
+    relevant_start = new Date(2018,4,20);
+    document.getElementById("boeing").innerText = dateDifference(boeing_start,today);
+    document.getElementById("tcs").innerText = dateDifference(tcs_start,tcs_end);
+    document.getElementById("total").innerText = dateDifference(tcs_start,today);
+    document.getElementById("total2").innerText = dateDifference(tcs_start,today);
+    document.getElementById("relevant").innerText = dateDifference(relevant_start,today);
+}
+
+function dateDifference(startDate,endDate){
+    var months;
+    months = endDate.getMonth() - startDate.getMonth() +  (12 * (endDate.getFullYear() - startDate.getFullYear()));
+    if(endDate.getDate() < startDate.getDate()){
+        months -=1;
+    }
+    var years = Math.floor(months / 12);
+    var extraMonths = months % 12;
+    return years +' years and ' +extraMonths +' months';
+}
+
+window.addEventListener("load", calculateExperience);
